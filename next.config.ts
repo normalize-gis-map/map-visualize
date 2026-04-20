49bb779664662736b0bd9ddb50d493a9eb7dd249
Bee Nguyen <nguyentribinh06011995@gmail.com>
2026-04-20 11:27:39 +0700

    fix lock dockerfile

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 
+  output: "standalone", 
+
   experimental: {
     optimizePackageImports: ["lucide-react"],
   },
